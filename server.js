bc8e6dc6f5dc9aff688a9ce360a8e28e6c620dc9
Nurbek <92941063+RasulovN@users.noreply.github.com>
2022-05-11 15:30:29 +0500

    Update server.js

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ const store = new MongoStore({
 })
 
 // Middleware
-app.use(express.json())
+app.use(express.json({ extended: false }));
 app.use(express.urlencoded({ extended: false }))
 
 app.use(session({
